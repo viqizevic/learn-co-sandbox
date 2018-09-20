17631d4a982fb5a528cbb17c64566a060025b543
viqi zevic <vicky.tanzil@gmail.com>
2018-09-20 08:12:27 +0000

    automatically backed up by learn

---
+++ sandbox.rb
@@ -0,0 +1,10 @@
+def greeting(language="Ruby", name)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting("Alex")
+
+
+array = ["Hello", "World"]
+puts "#{array.join(" ")}"
+# => Hello World
